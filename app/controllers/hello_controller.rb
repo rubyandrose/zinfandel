a2e546c266a912e8a563aefc9f08cb59f65ce1aa
Joe Jansen <josephjansen@gmail.com>
2017-05-30 19:47:00 -0400

    Add post route, added the ability to post and display a post

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,5 +2,6 @@ class HelloController < ApplicationController
   def index
     @picture = current_user.picture_url
     @user_name = current_user.full_name
+    @posts = Post.all
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    Post.new(user: current_user, content: params[:content]).save
+    redirect_to :back
+  end
 end
