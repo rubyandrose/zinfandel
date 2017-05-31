cd7f56e980dd1c0156e6d42f13c2ce61af94b0a7
Joe Jansen <josephjansen@gmail.com>
2017-05-30 20:15:20 -0400

    Improved create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,16 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.new(user: current_user, content: params[:content]).save
-    redirect_to :back
+    if content_params.present?
+      Post.new(user: current_user, content: content_params).save
+      redirect_to root_path
+    else
+      redirect_to root_path
+      ## figure out how to display an error message
+    end
+  end
+
+  def content_params
+    @content_params ||= params.permit(:content)[:content]
   end
 end
