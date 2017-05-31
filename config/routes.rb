c858efe5bc2009ac0d3da7bc8c343d8ca3b47a50
Han Loaner <hanloaner@Loaner-FoxMcCloud-MBP15.local>
2017-05-30 20:15:36 -0400

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
+  resources :users, only: [:show]
+
   root to: "hello#index"
 end
