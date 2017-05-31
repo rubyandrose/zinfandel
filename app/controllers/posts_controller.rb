33a44dca24d4992c0726766de6a95c41a98f4c30
Joe Jansen <josephjansen@gmail.com>
2017-05-30 20:51:26 -0400

    remove extra create route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,18 +13,4 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
-  def create
-    if content_params.present?
-      Post.new(user: current_user, content: content_params).save
-      redirect_to root_path
-    else
-      redirect_to root_path
-      ## figure out how to display an error message
-    end
-  end
-
-  def content_params
-    @content_params ||= params.permit(:content)[:content]
-  end
 end
