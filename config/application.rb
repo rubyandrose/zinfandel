a1762fb2cda170caf9b3dbc78ad4b580bff7c153
Adrian Bautista <adrianbautista@users.noreply.github.com>
2017-10-08 21:21:21 -0400

    Remove generators

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module RubyAndRose
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+    end
   end
 end
