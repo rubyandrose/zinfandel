6a9758a278cf74207fe04581db1d7dfe4fe6991b
Adrian Bautista <adrianbautista@users.noreply.github.com>
2017-10-15 19:09:26 -0400

    Remove PostController and add basic picture_url validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,13 @@ class User < ApplicationRecord
 
   has_many :posts
 
+  validates :picture_url, format: { with: /.*\.(jpg|png)/i, message: 'must end in an image format' }
+
   def self.from_omniauth(auth)
     find_or_create_by(email: auth.info.email).tap do |user|
       user.full_name = auth.info.name
       user.picture_url = auth.info.image
       user.save
     end
-  end
-
-  def picture_url
-    read_attribute(:picture_url) || avatar.url
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new }
+
+  describe 'validations' do
+    context 'given a picture_url that does not end in an image format' do
+      it "invalidates the user's picture_url" do
+        subject.picture_url = "folder/not_image.csv"
+        expect(subject.valid?).to eq false
+        expect(subject.errors[:picture_url]).to include 'must end in an image format'
+      end
+    end
+
+    context 'given a picture_url that ends in a JPG image format' do
+      it "does not invalidate the user's picture_url" do
+        subject.picture_url = "folder/image.jpg"
+        expect(subject.valid?).to eq true
+        expect(subject.errors[:picture_url]).to be_blank
+      end
+    end
+
+    context 'given a picture_url that ends in a PNG image format' do
+      it "does not invalidate the user's picture_url" do
+        subject.picture_url = "folder/image.png"
+        expect(subject.valid?).to eq true
+        expect(subject.errors[:picture_url]).to be_blank
+      end
+    end
+  end
 end
