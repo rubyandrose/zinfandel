f2c51c4b043eb8c491d12ff2e9ceb59aae60a628
Adrian Bautista <adrianbautista@users.noreply.github.com>
2017-10-15 19:16:45 -0400

    Add post validation test for TDD

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'given no content data' do
+      it 'is invalid' do
+        post = Post.new(content: nil)
+        expect(post.valid?).to eq false
+        expect(post.errors[:content]).to include "can't be blank"
+      end
+    end
+
+    context 'given no user to associate the post to' do
+      it 'is invalid' do
+        post = Post.new(user: nil)
+        expect(post.valid?).to eq false
+        expect(post.errors[:user]).to include "can't be blank"
+      end
+    end
+  end
 end
